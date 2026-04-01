ee3cb1162d50508cc99b48bb723ec9cd50bcced2
Gyarteng Otchere-Appiah <145002453+GGyartengApp@users.noreply.github.com>
2026-04-01 21:04:48 +0000

    Updated the main class where user input will be accepted and passed through the Matrix Class methods

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,25 @@
-public class Matrix{
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        Matrix matrix = new Matrix(n, n + 1);
+
+        System.out.println("Enter the augmented matrix:");
+        matrix.input(sc);
+
+        System.out.println("\nInitial Matrix:");
+        matrix.print();
+
+        GaussianElimination solver = new GaussianElimination();
+        solver.eliminate(matrix);
+
+        System.out.println("Final Matrix:");
+        matrix.print();
+    }
 }
